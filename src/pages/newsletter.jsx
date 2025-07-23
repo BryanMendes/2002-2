b0692a40e1ef6b2e995ff070726caf3b4777b680
BryanMendes <bryan.mendesmonteiro@gmail.com>
2025-07-22 23:58:17 -0300

    att4

--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Newsletter() {
   return (
-    <div style={{ minHeight: '100vh', background: '#1E392A', color: '#F0F0C0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.5rem', textAlign: 'center' }}>
+    <div style={{ minHeight: '100vh', background: '#1E392A', color: '#F0F0C0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.5rem', textAlign: 'center', paddingTop: '100px' }}>
       página em construção...
     </div>
   );
