343e21fd4c85792749f16e9a0a74d1a91a5522ce
BryanMendes <86127558+BryanMendes@users.noreply.github.com>
2025-07-22 23:09:41 -0300

    Fix Newsletter.jsx syntax error

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,23 +1,11 @@
-@@ .. @@
--import React from 'react';
--
--function Newsletter() {
--  return (
--    <div style={{ minHeight: '100vh', background: '#1E392A', color: '#F0F0C0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.5rem', textAlign: 'center' }}>
--      página em construção...
--    </div>
--  );
--}
--
--export default Newsletter;
-+import React from 'react';
-+
-+function Newsletter() {
-+  return (
-+    <div style={{ minHeight: '100vh', background: '#1E392A', color: '#F0F0C0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.5rem', textAlign: 'center', paddingTop: '100px' }}>
-+      página em construção...
-+    </div>
-+  );
-+}
-+
-+export default Newsletter;
+import React from 'react';
+
+function Newsletter() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#1E392A', color: '#F0F0C0', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '2.5rem', textAlign: 'center', paddingTop: '100px' }}>
+      página em construção...
+    </div>
+  );
+}
+
+export default Newsletter;
